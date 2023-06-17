bcf00350a79b0a2869e7095d3009ccd527c11059
siva <mangalisivapullaiah@gmail.com>
2023-06-17 22:09:28 +0530

    getting the output in input url feild chainged js

--- scripts/script.js
+++ scripts/script.js
@@ -1,28 +1,30 @@
-// main script will be here
-
 function shortenUrl() {
-    const longUrl = document.getElementById('longUrlInput').value;
-    
-    // Make an API request to Bitly using your preferred method (e.g., Fetch, Axios, etc.)
-    // Remember to replace YOUR_API_KEY with your actual Bitly API key
-    fetch(`https://api-ssl.bitly.com/v4/shorten`, {
+    const longUrlInput = document.getElementById('longUrlInput');
+    const shortUrlInput = document.getElementById('shortUrlInput');
+
+    const longUrl = longUrlInput.value;
+
+    const accessToken = '37b5e92787ddfb1e3d98a1b13c7d494ad77ea4a3';
+    const apiUrl = 'https://api-ssl.bitly.com/v4/shorten';
+
+    const data = {
+      long_url: longUrl
+    };
+
+    fetch(apiUrl, {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
-        'Authorization': 'Bearer 37b5e92787ddfb1e3d98a1b13c7d494ad77ea4a3'
+        'Authorization': `Bearer ${accessToken}`
       },
-      body: JSON.stringify({
-        long_url: longUrl
-      })
+      body: JSON.stringify(data)
     })
     .then(response => response.json())
-    .then(data => {
-      const shortenedUrl = data.link;
-      document.getElementById('shortenedUrl').textContent = `Shortened URL: ${shortenedUrl}`;
+    .then(response => {
+      const shortenedUrl = response.id;
+      shortUrlInput.value = shortenedUrl;
     })
     .catch(error => {
       console.error('Error:', error);
-      document.getElementById('shortenedUrl').textContent = 'An error occurred. Please try again.';
     });
   }
-  
