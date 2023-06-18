138a1c496418d40598109a20b3d9ced8507e16f0
siva <mangalisivapullaiah@gmail.com>
2023-06-18 07:23:06 +0530

    ease copy button added with js

--- scripts/copy.js
+++ scripts/copy.js
@@ -1,1 +1,18 @@
 // After getting the shortern URL simple copy will be present here
+
+document.querySelectorAll(".copy-link").forEach((copyLinkParent) => {
+    const inputField = copyLinkParent.querySelector(".copy-link-input");
+    const copyButton = copyLinkParent.querySelector(".copy-link-button");
+  
+    inputField.addEventListener("focus", () => inputField.select());
+  
+    copyButton.addEventListener("click", () => {
+      const text = inputField.value; // Move text assignment inside the click event listener
+      inputField.select();
+      navigator.clipboard.writeText(text);
+  
+      inputField.value = "Copied!";
+      setTimeout(() => (inputField.value = text), 2000);
+    });
+  });
+  
